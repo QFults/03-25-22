7a1ab2f9923fa5ca0546671a43778488744b7d64
Quinton Fults <quinton.fults.work@gmail.com>
2022-03-25 11:27:02 -0700

    code

--- controllers/hotdogRoutes.js
+++ controllers/hotdogRoutes.js
@@ -1,3 +1,23 @@
 const router = require('express').Router()
+const { Hotdog } = require('../models')
+
+router.post('/hotdogs', async (req, res) => {
+  try {
+    const hotdog = await Hotdog.create(req.body)
+    res.status(200).json(hotdog)
+  } catch (err) {
+    res.status(500).json({ message: 'Something went wrong, please try again.', error: err })
+  }
+})
+
+router.get('/hotdogs/:id', async ({ params: { id } }, res) => {
+  try {
+    let hotdog = await Hotdog.findOne({ where: { id } })
+    hotdog = hotdog.get({ plain: true })
+    res.render('hotdog', hotdog)
+  } catch (err) {
+    res.status(500).json({ message: 'Something went wrong, please try again.', error: err })
+  }
+})
 
 module.exports = router
